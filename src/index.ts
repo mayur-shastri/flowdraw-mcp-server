36d921fb865050941e2bbeaffd1b710ade81e92a
Mau_2993 <shastrimayur2993@gmail.com>
2025-08-01 20:15:41 +0530

    Add: test route

--- src/index.ts
+++ src/index.ts
@@ -15,7 +15,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.get('/', (req, res)=>{
-    res.json({message: "Hello World"});
+    res.json({message: "Hello World MCP"});
 });
 
 app.use('/diagram', diagramRouter);
